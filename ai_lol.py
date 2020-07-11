5c5f616783ae42ff041e0bbed24093ab0b58bbf9
Salixe1214 <alexis.st-amand.1@ulaval.ca>
2020-07-11 16:10:57 -0400

    Réagit à l'amour

--- ai_lol.py
+++ ai_lol.py
@@ -2,6 +2,12 @@
 # NOTE: this example requires PyAudio because it uses the Microphone class
 import speech_recognition as sr
 from jokes import randJk
+import pyttsx3
+
+# Settings pour tts
+engine = pyttsx3.init()
+voice = engine.getProperty('voices')[1] # the french voice
+engine.setProperty('voice', voice.id)
 
 # obtain audio from the microphone
 r = sr.Recognizer()
@@ -19,17 +25,13 @@ except sr.RequestError as e:
     print("Sphinx error; {0}".format(e))
 
 
-if any(word in text.split() for word in ["blague", "histoire drôle", "marrant"]):
-    blague = "Il était une fois...\nAh! pis d'la marde!"
-
-    import pyttsx3
-    engine = pyttsx3.init()
-    voice = engine.getProperty('voices')[1] # the french voice
-
-    engine.setProperty('voice', voice.id)
+if any(word in text for word in ["blague", "histoire drôle", "marrant"]):
     joke = randJk()
     print(joke)
-
     engine.say(joke) # perfect # it works!!
 
-    engine.runAndWait()
+if any(word in text for word in ["je t'aime", "tu es un amours", "bisous"]):
+    engine.say("C'est gênant")
+    print("\n\n\n(⁄ ⁄•⁄ω⁄•⁄ ⁄)⁄")
+
+engine.runAndWait()
