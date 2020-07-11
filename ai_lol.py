1e440bba9b4aca6a8e1374eca3f5bc4b5f346b21
Salixe1214 <alexis.st-amand.1@ulaval.ca>
2020-07-11 13:29:30 -0400

    dsa

--- ai_lol.py
+++ ai_lol.py
@@ -1,1 +1,27 @@
-dsadsad
+#import library
+import speech_recognition as sr
+
+# Initialize recognizer class (for recognizing the speech)
+r = sr.Recognizer()
+
+# Reading Audio file as source
+# listening the audio file and store in audio_text variable
+
+
+with sr.Microphone() as source:
+    x = 0
+    while x < 5:
+
+        audio_text = r.listen(source)
+
+    # recoginize_() method will throw a request error if the API is unreachable, hence using exception handling
+        try:
+
+            # using google speech recognition
+            text = r.recognize_sphinx(audio_text)
+            print(text)
+
+        except:
+             print('Sorry.. run again...')
+
+        x = x + 1
